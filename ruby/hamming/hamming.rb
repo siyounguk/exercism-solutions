298ca750748a532004f38bf98d16a4a2ecba150a
siyounguk <si.younguk@gmail.com>
2016-01-20 18:57:05 +0000

    initial commit

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,16 @@
+class Hamming
+  def self.compute (strand_one, strand_two)
+    
+    (strand_one.chars-strand_two.chars).count  
+
+
+    # strand_one_each = strand_one_arr.each{|x|
+    # }
+
+    # if strand_one - strand_two
+    #   assert_equal = 0
+    # elsif strand_one != strand_two
+    #   assert_equal = 1
+    # end 
+  end
+end
