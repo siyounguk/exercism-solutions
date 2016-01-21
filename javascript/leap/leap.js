8ba185d3a04204e61b110c91adf5ec2b9e913667
siyounguk <si.younguk@gmail.com>
2016-01-21 16:45:49 +0000

    fetched exercism tasks

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -4,10 +4,9 @@ var Year = function(year){
 
 Year.prototype.isLeap = function(){
   if((this.year % 4 === 0) && (this.year % 100 != 0) || (this.year % 400 == 0)){
-    return true
+    return true;
   }else{
-    return false
+    return false;
   }  
 }
-
 module.exports = Year;
