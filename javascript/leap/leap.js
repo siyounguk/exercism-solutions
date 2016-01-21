29ca045eb77a79885dbee2334193d02233bf1433
siyounguk <si.younguk@gmail.com>
2016-01-21 16:56:26 +0000

    removed if statement from isLeap code

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,12 +1,8 @@
 var Year = function(year){
-  this.year = year
+  this.year = year;
 }
 
 Year.prototype.isLeap = function(){
-  if((this.year % 4 === 0) && (this.year % 100 != 0) || (this.year % 400 == 0)){
-    return true;
-  }else{
-    return false;
-  }  
+  return (this.year % 4 === 0) && (this.year % 100 != 0) || (this.year % 400 == 0);
 }
 module.exports = Year;
