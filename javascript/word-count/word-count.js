ab24a31dc2ab90c18806017d3a21af663abfe74c
siyounguk <si.younguk@gmail.com>
2016-01-28 19:18:01 +0000

    word count submissions, final test to pass

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,33 @@
+var Words = function(){
+}
+
+Words.prototype.count = function(string) {
+  string = string.replace(/(\r\n|\n|\r|\t|\s\s)/gm," ");
+  string = string.trim()
+
+  stringArray = string.split(" ");
+  var counts = {};
+  var wordsArray = [];
+  var numArray = [];
+
+  for(i =0; i< stringArray.length; i++){
+
+    var num = stringArray[i];
+    counts[num] = counts[num] ? counts[num]+1 : 1;
+    
+    wordsArray.push(stringArray[i]);
+    numArray.push(counts[num]);
+  }
+
+  makeObject = function (keys, vals) {
+    return object = keys.reduce (
+      function(prev, val, i) { prev[val] = vals[i]; return prev; }, 
+        {});
+  }
+
+  makeObject(wordsArray, numArray);
+  return object;
+
+};
+
+module.exports = Words;
